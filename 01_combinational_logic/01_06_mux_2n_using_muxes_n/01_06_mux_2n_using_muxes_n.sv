ed25c44dfd3c49a1ee12ccf3fc30a4e4d02af32e
nkolobov92-wq <nkolobov92@gmail.com>
2025-10-26 20:41:29 +0300

    Update 01_06_mux_2n_using_muxes_n.sv

--- 01_combinational_logic/01_06_mux_2n_using_muxes_n/01_06_mux_2n_using_muxes_n.sv
+++ 01_combinational_logic/01_06_mux_2n_using_muxes_n/01_06_mux_2n_using_muxes_n.sv
@@ -24,6 +24,11 @@ module mux_4_1
   output [3:0] y
 );
 
+  wire [3:0] out0, out1;
+
+  mux_2_1 mux0 (.d0(d0), .d1(d1), .sel(sel[0]), .y(out0));
+  mux_2_1 mux1 (.d0(d2), .d1(d3), .sel(sel[0]), .y(out1));
+  mux_2_1 mux2 (.d0(out0), .d1(out1), .sel(sel[1]), .y(y));
   // Task:
   // Implement mux_4_1 using three instances of mux_2_1
 
