582069e608386868fcaf074b74f08691d2149440
nkolobov92-wq <nkolobov92@gmail.com>
2025-10-26 20:59:44 +0300

    Update 01_11_xor_gate_using_mux.sv

--- 01_combinational_logic/01_11_xor_gate_using_mux/01_11_xor_gate_using_mux.sv
+++ 01_combinational_logic/01_11_xor_gate_using_mux/01_11_xor_gate_using_mux.sv
@@ -24,6 +24,21 @@ module xor_gate_using_mux
     output o
 );
 
+    wire not_b;
+  
+  mux not_b_mux (
+    .d0(1'b1),  
+    .d1(1'b0),  
+    .sel(b),    
+    .y(not_b)  
+  );
+  
+  mux xor_mux (
+    .d0(b),     
+    .d1(not_b), 
+    .sel(a),    
+    .y(o)      
+  );
   // Task:
   // Implement xor gate using instance(s) of mux,
   // constants 0 and 1, and wire connections
