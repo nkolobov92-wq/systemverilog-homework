d8c1afba61d7974f0bc4cfb6ffa5e1c99cf390ea
nkolobov92-wq <nkolobov92@gmail.com>
2025-10-26 20:53:37 +0300

    Update 01_08_not_gate_using_mux.sv

--- 01_combinational_logic/01_08_not_gate_using_mux/01_08_not_gate_using_mux.sv
+++ 01_combinational_logic/01_08_not_gate_using_mux/01_08_not_gate_using_mux.sv
@@ -23,6 +23,12 @@ module not_gate_using_mux
     output o
 );
 
+  mux not_mux (
+    .d0(1'b1),  
+    .d1(1'b0),  
+    .sel(i),     
+    .y(o)        
+  );
   // Task:
   // Implement not gate using instance(s) of mux,
   // constants 0 and 1, and wire connections
